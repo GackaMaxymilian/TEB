4652970a52b2293e4c0c6d2d8ca532f50a2d6f4c
GackaMaxymilian <92211476+GackaMaxymilian@users.noreply.github.com>
2021-10-09 14:53:42 +0200

    Add files via upload

---
+++ 2_PHP/1_index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Strona głównawbr</title>
+  </head>
+  <body>
+<p>Strona główna<p>
+
+  </body>
+</html>
